db0b9bbcd714c4fbaf7bc9cbf206310a0c0dcd60
yash-1104github <gyah1104@gmail.com>
2025-10-25 19:52:23 +0530

    chore: ADDED PROD URL

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["https://trans-yobe.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 }));
