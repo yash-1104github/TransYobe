91562ff63770186a46086d31b63f5084f0cc97f5
yash-1104github <gyah1104@gmail.com>
2025-10-31 20:54:28 +0530

    fix: rebase error

--- backend/src/controllers/TranscriptController.ts
+++ backend/src/controllers/TranscriptController.ts
@@ -2,10 +2,6 @@ import { extractVideoId } from "../utils/extractVideoId.js";
 import { Request, Response, NextFunction } from "express";
 import { fetchTranscript } from "youtube-transcript-plus";
 import { ensureIndex, loadSampleData } from "../RAG_piplines/dataIngesion.js";
-import {
-  createCollection,
-  loadSampleData,
-} from "../RAG_piplines/dataIngesion.js";
 
 interface types {
   item: any;
@@ -14,7 +10,7 @@ interface types {
 }
 
 export default async function handleTranscript(req: Request, res: Response) {
-   const { youtubeUrl }: any = req.body;
+  const { youtubeUrl }: any = req.body;
 
   if (!youtubeUrl) {
     return res.status(400).json({ error: "Missing Youtube url." });
@@ -27,33 +23,27 @@ export default async function handleTranscript(req: Request, res: Response) {
   }
 
   try {
-    console.log('inside try block of controller transcript');
     console.log(`Fetching transcript for video: ${videoId}`);
-    const response = await fetch(`https://trans-yobe.vercel.app/api/getTranscript?videoId=${videoId}`);
-    console.log("response", response);
-    
-    if (!response.ok) {
-      return res.status(response.status).json({ error: "Failed to fetch transcript" });
-    }
 
-    const data = await response.json();
-    const transcript = data.transcript;
+    const transcript = await fetchTranscript(videoId, { lang: "en" });
 
     if (transcript.length > 0) {
-      const propertranscript = transcript.map((item: types) => item.text).join(" ");
+      const propertranscript = transcript
+        .map((item: types) => item.text)
+        .join(" ");
 
       const finaltranscript = propertranscript
         .replace(/&amp;/g, "&")
-        .replace(/&#39;/g, "'") 
-        .replace(/\\n/g, " ") 
-        .replace(/\n/g, " ") 
-        .replace(/\s+/g, " ") 
-        .trim(); 
+        .replace(/&#39;/g, "'")
+        .replace(/\\n/g, " ")
+        .replace(/\n/g, " ")
+        .replace(/\s+/g, " ")
+        .trim();
 
-      console.log("Fetched Transcipt from api");
-   
-      // await ensureIndex();
-      console.log("Index created");
+      console.log("Fetched Transcipt from api", finaltranscript);
+
+      // await createCollection();
+      console.log("Collection created");
 
       await loadSampleData(finaltranscript);
 
