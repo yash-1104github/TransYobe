c880d5477dbf394afef9a416fbf5ed8d68a64d5e
yash-1104github <gyah1104@gmail.com>
2025-11-01 21:06:40 +0530

    chore: Update

--- backend/src/controllers/progress.ts
+++ backend/src/controllers/progress.ts
@@ -3,6 +3,14 @@ import { registerClient } from "../utils/sse";
 
 export default function progress(req : Request, res: Response) {
 
+   if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", "https://trans-yobe.vercel.app");
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.status(204).end();
+    return;
+  }
+
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
